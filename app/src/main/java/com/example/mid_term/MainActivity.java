0462fc51bfd7cc5a84fe65adeb7b3df1345da194
LENOVO <sachinsinghey987@gmail.com>
2024-07-10 12:19:19 +0545

    Initial Commit

---
+++ app/src/main/java/com/example/mid_term/MainActivity.java
@@ -0,0 +1,95 @@
+package com.example.mid_term;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    Button alertButton, buttonSend;
+    firstFragment fragment1;
+    secondFragment fragment2;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.edit_text);
+        buttonSend = findViewById(R.id.button1_id);
+
+        textView = findViewById(R.id.text_view);
+
+        if(savedInstanceState==null){
+
+            fragment1 = new firstFragment();
+            fragment2 = new secondFragment();
+
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fm.beginTransaction();
+            fragmentTransaction.add(R.id.first_frag, fragment1);
+            fragmentTransaction.add(R.id.second_frag, fragment2);
+
+            fragmentTransaction.commit();
+
+        }
+
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = editText.getText().toString();
+                fragment1.receiveData(text);
+                fragment2.receiveData(text);
+            }
+        });
+
+        alertButton = findViewById(R.id.button2_id);
+
+        alertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder alertDialogBox = new AlertDialog.Builder(MainActivity.this);
+                alertDialogBox.setTitle("Alert Dialog Box");
+                alertDialogBox.setMessage("Navigate to another activity");
+                alertDialogBox.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Create an intent to start the new activity
+                        Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                        // Start the new activity
+                        startActivity(intent);
+                    }
+                });
+
+                alertDialogBox.create();
+                alertDialogBox.show();
+
+
+            }
+        });
+
+        return false;
+    }
+
+    public void updateTextView(String data){
+        textView.setText(data);
+    }
+}
+    }
+}
---
+++ app/src/main/java/com/example/mid_term/firstFragment.java
@@ -0,0 +1,96 @@
+package com.example.mid_term;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link firstFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class firstFragment extends Fragment {
+
+
+    TextView textView;
+
+    EditText editTextEmail, editTextPassword;
+    Button button;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public firstFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment firstFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static firstFragment newInstance(String param1, String param2) {
+        firstFragment fragment = new firstFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_first, container, false);
+
+        editTextEmail = v.findViewById(R.id.email_input);
+        editTextPassword = v.findViewById(R.id.password_input);
+        button = v.findViewById(R.id.submit_button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = editTextEmail.getText().toString();
+                String password = editTextPassword.getText().toString();
+                String data = "Email: " + email + "\nPassword: " + password;
+                ((MainActivity) getActivity()).updateTextView(data);
+
+            }
+        });
+
+        textView = v.findViewById(R.id.text_view);
+        return v;
+    }
+
+    public void receiveData(String text){
+        textView.setText(text);
+    }
+
+}
